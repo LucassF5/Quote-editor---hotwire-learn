e4355e35b1bd75a7ffe74d936082f3f247a1ec52
Lucas F <franco100lucas@gmail.com>
2025-01-25 15:53:04 -0300

    Feat: Add Turbo Streams for real-time updates on quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,11 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  # after_create_commit  -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit  -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+  # The above callbacks can be replaced with the following:
+  broadcast_to ->(quote) { "quotes" }, insert_bt: :prepend
 end
