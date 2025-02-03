5da91cf90550d1a036ae502a6ddd4fa675929f04
Lucas F <franco100lucas@gmail.com>
2025-02-03 09:46:07 -0300

    Feat: Add edit, update, and destroy actions for line item dates with Turbo support

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -1,5 +1,6 @@
 class LineItemDatesController < ApplicationController
   before_action :set_quote
+  before_action :set_line_item_date, only: [ :edit, :update, :destroy ]
 
   def new
     @line_item_date = @quote.line_item_dates.build
@@ -9,16 +10,40 @@ class LineItemDatesController < ApplicationController
     @line_item_date = @quote.line_item_dates.build(line_item_date_params)
 
     if @line_item_date.save
-      redirect_to quote_path(@quote), notice: "Date was successfully created."
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Date was successfully created." }
+        format.turbo_stream { flash.now[:notice] = "Date was successfully created." }
+      end
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @line_item_date.update(line_item_date_params)
+      redirect_to quote_path(@quote), notice: "Date was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @line_item_date.destroy
+
+    redirect_to quote_path(@quote), notice: "Data was successfully destroyed"
   end
 
   private
 
   def line_item_date_params
     params.expect(line_item_date: [ :date ])
+  end
+
+  def set_line_item_date
+    @line_item_date = @quote.line_item_dates.find(params[:id])
   end
 
   def set_quote
